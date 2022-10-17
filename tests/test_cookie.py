b3384a4f8a42df51f60141a66144ee2ee30248f7
ibashirov <i.bashirov@quality-lab.ru>
2022-10-17 13:49:38 +0300

    Ex12: Тест запроса на метод header

--- tests/test_cookie.py
+++ tests/test_cookie.py
@@ -2,8 +2,8 @@ import requests
 
 
 class TestCookie:
-  def test_cookie(self):
-    response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
-    cookie = dict(response.cookies)
-    print(cookie)
-    assert cookie['Homework'] == 'hm_value'
+    def test_cookie(self):
+        response = requests.get('https://playground.learnqa.ru/api/homework_cookie')
+        cookie = dict(response.cookies)
+        print(cookie)
+        assert cookie['HomeWork'] == 'hw_value'
--- tests/test_short_phrase.py
+++ tests/test_short_phrase.py
@@ -2,5 +2,5 @@ import pytest
 class TestShortPhrase:
 
     def test_short_phrase(self):
-        phrase ="sdfgsd" #input("Введите фразу короче 15 символов:")
+        phrase ="sdfgsdolkjlkjjhljkkjhlhlljhl" #input("Введите фразу короче 15 символов:")
         assert len(phrase) < 15, "Фраза длинее 15 символов"
