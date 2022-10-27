3673b2c983fbdb72e313bd0ca8564eba0e570bc1
ibashirov <i.bashirov@quality-lab.ru>
2022-10-27 17:35:40 +0300

    Ex14: Формирование фреймворка- удалены лишние тесты, test_user_auth.py перемещен в папку tests

--- tests/test_cookie.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-class TestCookie:
-    def test_cookie(self):
-        response = requests.get('https://playground.learnqa.ru/api/homework_cookie')
-        cookie = dict(response.cookies)
-        print(cookie)
-        assert cookie['HomeWork'] == 'hw_value'
--- tests/test_short_phrase.py
+++
@@ -1,6 +0,0 @@
-import pytest
-class TestShortPhrase:
-
-    def test_short_phrase(self):
-        phrase ="sdfgsdolkjlkjjhljkkjhlhlljhl" #input("Введите фразу короче 15 символов:")
-        assert len(phrase) < 15, "Фраза длинее 15 символов"
